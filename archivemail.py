62f2d394de8450725131a529c58541bfc84396c1
Harish Murthy <harish.v.murthy@gmail.com>
2015-07-28 06:53:27 +0530

    fix archive issues

--- archivemail.py
+++ archivemail.py
@@ -2,12 +2,13 @@
 
 
 import sys
-from settings import r
+from redis import StrictRedis
 from bson import Binary
 import pymongo
+import pickle
 
 if __name__ == '__main__':
-  instance = sys.argv[2]
+  instance = sys.argv[-1]
 
   if not instance:
     instance = "1"
@@ -15,13 +16,16 @@ if __name__ == '__main__':
   try:
     conn=pymongo.MongoClient()
     print ("Connected successfully!!!")
-  except pymongo.errors.ConnectionFailure, e:
+  except pymongo.errors.ConnectionFailure as e:
     print ("Could not connect to MongoDB: %s" % e )
 
   db = conn.inbounddb.mailBackup
+  r = StrictRedis()
 
   while True:
     item = r.brpoplpush('mailarchive', 'mailarchivebackup')
+    item = pickle.loads(item)
+    print('item ' + str(item))
     db.insert( item[0] )
     print ('len of mailarchivebackup is : {}'.format(r.llen('mailarchivebackup')))
     r.lrem('mailarchivebackup', 0, item)
